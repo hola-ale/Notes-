bbaa408282008a9cd2bc99dbd6d7e6193f9f7db0
Ola Hola <homoncik.aleksandra@gmail.com>
2022-01-05 15:29:01 +0100

    add editor's behavior

--- src/Components/Editor/Editor.js
+++ src/Components/Editor/Editor.js
@@ -1,7 +1,29 @@
-import react from "react";
+import React from "react";
 import ReactMde from "react-mde";
 import Showdown from "showdown";
 
 export default function Editor({currentNote, updateNote}) {
-    
+    cost [selectedTab, setselectedTab] = React.useState("write")
+    const converter = new Showdown.Converter({
+        tables: true,
+        simplifiedAutoLink: true,
+        strikethrough: true,
+        tasklists: true,
+    })
+
+    return (
+        <section className="editor-panel">
+            <ReactMde
+                value={currentNote.body}
+                onChange={updateNote}
+                selectedTab={selectedTab}
+                onTabChange={setSelectedTab}
+                generateMarkdownPreview={(markdown) =>
+                    Promise.resolve(converter.makeHtml(markdown))
+                }
+                minEditorHeight={80}
+                heightUnits="vh"
+            />
+        </section>
+    )
 }
