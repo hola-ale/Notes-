edc63a1d2ac34fd932ff28882eeafdf0591f1645
Ola Hola <homoncik.aleksandra@gmail.com>
2022-01-05 15:24:05 +0100

    add basic functions to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,85 @@
 import React from "react";
-import Split from "react-split"
+import Split from "react-split";
+import {nanoid} from "nanoid";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 export default function App() {
+  const [notes, setNotes] = React.useState(
+    () => JSON.parse(localStorage.getItem("notes")) || []
+  )
+  const [currentNoteId, setCurrentNoteId] = React.useState(
+    (notes[0] && notes[0].id) || ""
+  )
+  React.useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes))
+  }, [notes])
+
+  function createNewNote() {
+    const newNote = {
+      id: nanoid(),
+      body: "Your note goes here"
+    }
+    setNotes(prevNotes => [newNote, ...prevNotes])
+    setCurrentNoteId(newNote.id)
+  }
+
+  function updateNote(bodytext) {
+    setNotes(oldNotes => {
+      const newNotesArray = []
+      for(let i=0; i < oldNotes.size; i++) {
+        const oldNote = oldNotes[i]
+        if(oldNote.id === currentNoteId) {
+          newNotesArray.unshift({...oldNote, body: bodytext})
+        } else {
+          newNotesArray.push(oldNote)
+        }
+      }
+      return newNotesArray
+    })
+  }
+
+  function findCurrentNote() {
+    return notes.find(note => {
+      return note.id === currentNoteId
+    }) || notes[0]
+  }
+
   return (
     <div className="App">
-      <Split />
+      {
+        notes.size > 0
+        ?
+        <Split
+          sizes={[30, 70]} 
+          direction="horizontal" 
+          className="split"
+        >
+          <Sidebar
+            notes={notes}
+            currentNote={findCurrentNote()}
+            setCurrentNoteId={setCurrentNoteId}
+            newNote={newNote}
+          />
+          {
+            currentNoteId 
+            && notes.size > 0 
+            && <Editor
+              currentNote={findCurrentNote()}
+              updateNote={updateNote}
+            />
+          }
+        </Split>
+        :
+        <div className="no-notes">
+          <h1>You have no notes</h1>
+          <button 
+              className="first-note" 
+              onClick={createNewNote}
+          >
+              Create one now
+          </button>
+        </div>
+      }
     </div>
   );
 }
---
+++ src/Components/Editor/Editor.js
@@ -0,0 +1,7 @@
+import react from "react";
+import ReactMde from "react-mde";
+import Showdown from "showdown";
+
+export default function Editor({currentNote, updateNote}) {
+    
+}
